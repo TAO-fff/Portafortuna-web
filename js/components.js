4a79323c66118762815a96c1b8dfb388af9cfea1
TAO-fff <134177125+TAO-fff@users.noreply.github.com>
2025-02-12 00:33:07 +0900

    voice-layout

--- js/components.js
+++ js/components.js
@@ -1,8 +1,8 @@
  // ハンバーガー
- $(function () {
-    $(".sp-menu-btn").on("click", function () {
-      $(this).toggleClass("open");
-      $(".header-nav-sp").slideToggle(300);  // 300msで開閉アニメーション
-    });
-  });
+//  $(function () {
+//     $(".sp-menu-btn").on("click", function () {
+//       $(this).toggleClass("open");
+//       $(".header-nav-sp").slideToggle(300);  // 300msで開閉アニメーション
+//     });
+//   });
   
--- js/voice.js
+++ js/voice.js
@@ -1,29 +1,32 @@
-
- // home voice スライダー
- $(function () {
-    $(".voice__gallery-slider-list").slick({
-      slidesToShow: 5,
-      slidesToScroll: 2,
-      arrows: true,
-      dots: true,
-      pauseOnFocus: false,//フォーカスで一時停止
-      pauseOnHover: false,//マウスホバーで一時停止
-      pauseOnDotsHover: false,//ドットナビをマウスホバーで一時停止
-      centerMode: false,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            autoplay: true,
-            autoPlaySpeed: 1000,
-            touchMove: true
-          },
+// voice gallery スライダー
+$(function () {
+  $(".voice__gallery-slider-list").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    infinite: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          arrows: true,
         },
-      ],
-    });
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
   });
+});
