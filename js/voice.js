062a8b0e75044a82ea4277f653a1f2b287a59079
TAO-fff <134177125+TAO-fff@users.noreply.github.com>
2025-02-13 12:40:24 +0900

    voice-animation

--- js/voice.js
+++ js/voice.js
@@ -1,32 +1,24 @@
 // voice gallery スライダー
-$(function () {
-  $(".voice__gallery-slider-list").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          arrows: true,
-        },
+$(window).on("load", function () {
+  setTimeout(function () {
+    $(".voice__gallery-slider-list").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true,
+      infinite: true,
+      pauseOnFocus: false,
+      pauseOnHover: false,
+      pauseOnDotsHover: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      appendArrows: ".voice__gallery-slider-arrows", // 独自のarrowコンテナを指定
+      appendDots: ".voice__gallery-slider-dots", // 独自のdotsコンテナを指定
+      customPaging: function (slider, i) {
+        // dotsのカスタムHTML
+        return '<span class="voice__dot"></span>';
       },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-        },
-      },
-    ],
-  });
+    });
+    console.log("Slick initialized with delay");
+  }, 500); // 500ミリ秒待つ
 });
