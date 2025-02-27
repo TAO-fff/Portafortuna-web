dffd5bfc33333b72a74fb400c422340752a9cef2
TAO-fff <134177125+TAO-fff@users.noreply.github.com>
2025-02-28 01:55:43 +0900

    hum-js

--- js/components.js
+++ js/components.js
@@ -1,58 +1,12 @@
 // humメニュー
 $(function () {
-  $(".header-nav-sp").css({ visibility: "hidden", opacity: 0 });
-
   $(".sp-menu-btn").on("click", function () {
     $(this).toggleClass("open");
-
-    if ($(this).hasClass("open")) {
-      $(".header-nav-sp")
-        .css({ visibility: "visible" })
-        .stop()
-        .animate({ opacity: 1 }, 300);
-    } else {
-      $(".header-nav-sp")
-        .stop()
-        .animate({ opacity: 0 }, 300, function () {
-          $(this).css({ visibility: "hidden" });
-        });
-    }
-  });
-});
-
-// スクロールイベント
-$(window).on("scroll", function () {
-  const scrollTop = $(this).scrollTop();
-  const windowHeight = $(window).height();
-  const spMenu = $(".sp-menu-btn");
-
-  let isWhite = false; // 白にするかどうかのフラグ
-
-  // 判定する要素
-  const targets = [
-    ".individual-top",
-    ".header-nav-sp",
-    ".home__reason",
-    ".reserve__container",
-    ".footer"
-  ];
-
-  targets.forEach((selector) => {
-    const target = $(selector);
-    if (target.length) {
-      const targetTop = target.offset().top;
-      const targetBottom = targetTop + target.outerHeight();
-
-      // sp-menu-btn がターゲットの範囲内にある場合
-      if (scrollTop + spMenu.height() >= targetTop && scrollTop <= targetBottom) {
-        isWhite = true;
-      }
-    }
+    $(".header-nav-sp").toggleClass("open");
   });
 
-  if (isWhite) {
-    spMenu.addClass("white-bg").removeClass("scrolled");
-  } else {
-    spMenu.addClass("scrolled").removeClass("white-bg");
-  }
+  $(".header-nav-sp a").on("click", function () {
+    $(".sp-menu-btn").removeClass("open");
+    $(".header-nav-sp").removeClass("open");
+  });
 });
